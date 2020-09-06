76190b3527724be1d37a68a3660a815f3a5e0697
Gonzalo <gonzisteel@gmail.com>
2020-09-06 11:10:41 -0500

    dump

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,323 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="white"
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="./assets/imateq.png"
+          transition="scale-transition"
+          width="200"
+          @click="pushAndResetNav('/')"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+      <v-btn @click="pushAndResetNav('/about')" text color="#0C5AA2" class="page-subtitle text-capitalize">
+        <span>Quienes Somos</span>
+      </v-btn>
+      <v-btn @click="pushAndResetNav('/contact')" text color="#0C5AA2" class="text-capitalize page-subtitle">
+        <span>Contacto</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-layout row wrap px-10 style="background-color: #F6F6F6">
+      <v-flex xs12>
+        <v-divider horizontal></v-divider>
+      </v-flex>
+      <v-flex xs12 sm6 md4 text-center class="my-auto page-subtitle" py-10>
+        <v-flex xs12>
+          <v-icon large color="#0C5AA2">mdi-home</v-icon>
+          <v-flex xs7 pt-2 class="mx-auto">
+            <v-divider horizontal></v-divider>
+          </v-flex>
+        </v-flex>
+        <v-flex xs12 pt-10>
+          <p>16 de Septiembre #915 ote</p>
+          <p>Col. Independencia, Monterrey, N.L.</p>
+        </v-flex>
+      </v-flex>
+      <v-flex xs12 sm6 md4 text-center  class="my-auto page-subtitle" py-10>
+        <v-flex xs12>
+          <v-icon large color="#0C5AA2">mdi-phone</v-icon>
+          <v-flex xs7 pt-2 class="mx-auto">
+            <v-divider horizontal></v-divider>
+          </v-flex>
+        </v-flex>
+        <v-flex xs12 pt-10>
+          <v-flex xs12>
+            <span>8190.4287</span>
+          </v-flex>
+          <v-flex xs12>
+            <span>8114.3187</span>
+          </v-flex>
+          <v-flex xs12>
+            <span>8114.3188</span>
+          </v-flex>
+        </v-flex>
+      </v-flex>
+      <v-flex xs12 sm6 md4 text-center class="mx-auto my-auto page-subtitle" py-10>
+        <v-flex xs12>
+          <v-icon large color="#0C5AA2">mdi-email</v-icon>
+          <v-flex xs7 pt-2 class="mx-auto">
+            <v-divider horizontal></v-divider>
+          </v-flex>
+        </v-flex>
+        <v-flex xs12 pt-10>
+          ventas@imateq.mx
+        </v-flex>
+        <v-flex xs12 pt-5>
+          <v-btn tile depressed dark color="#0C5AA2">
+            Contactanos
+          </v-btn>
+        </v-flex>
+      </v-flex>
+      <v-flex pt-10 xs12 text-center pt-2>
+        <a style="text-decoration: none;"  href="https://www.facebook.com">
+          <v-icon x-large color="#0C5AA2">mdi-facebook</v-icon>
+        </a>
+        <a style="text-decoration: none;"  href="https://www.instagram.com">
+          <v-icon x-large color="#0C5AA2">mdi-instagram</v-icon>
+        </a>
+        <a style="text-decoration: none;"  href="https://www.whatsapp.com">
+          <v-icon x-large color="#0C5AA2">mdi-whatsapp</v-icon>
+        </a>
+      </v-flex>
+      <v-flex xs12 text-center class="page-subtitle">
+        COPYRIGHT IMATEQ 2020 | ALL RIGHTS RESERVED
+      </v-flex>
+      <v-flex xs12>
+        <v-divider horizontal></v-divider>
+      </v-flex>
+    </v-layout>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+  methods: {
+    pushAndResetNav (route) {
+      if (this.$route.path !== route) {
+        this.$router.push(route)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+@font-face {
+  font-display: fallback;
+  font-family: 'Poppins';
+  src: url('./assets/fonts/Poppins-SemiBoldItalic.woff2') format('woff2'),
+      url('./assets/fonts/Poppins-SemiBoldItalic.woff') format('woff');
+  font-weight: 600;
+  font-style: italic;
 }
 
-#nav {
-  padding: 30px;
+@font-face {
+    font-display: fallback;
+    font-family: 'Poppins';
+    src: url('./assets/fonts/Poppins-ExtraBold.woff2') format('woff2'),
+        url('./assets/fonts/Poppins-ExtraBold.woff') format('woff');
+    font-weight: 800;
+    font-style: normal;
 }
 
-#nav a {
+@font-face {
+    font-display: fallback;
+    font-family: 'Poppins';
+    src: url('./assets/fonts/Poppins-Black.woff2') format('woff2'),
+        url('./assets/fonts/Poppins-Black.woff') format('woff');
+    font-weight: 900;
+    font-style: normal;
+}
+
+@font-face {
+    font-display: fallback;
+    font-family: 'Poppins';
+    src: url('./assets/fonts/Poppins-LightItalic.woff2') format('woff2'),
+        url('./assets/fonts/Poppins-LightItalic.woff') format('woff');
+    font-weight: 300;
+    font-style: italic;
+}
+
+@font-face {
+    font-display: fallback;
+    font-family: 'Poppins';
+    src: url('./assets/fonts/Poppins-ExtraBoldItalic.woff2') format('woff2'),
+        url('./assets/fonts/Poppins-ExtraBoldItalic.woff') format('woff');
+    font-weight: 800;
+    font-style: italic;
+}
+
+@font-face {
+    font-display: fallback;
+    font-family: 'Poppins';
+    src: url('./assets/fonts/Poppins-ThinItalic.woff2') format('woff2'),
+        url('./assets/fonts/Poppins-ThinItalic.woff') format('woff');
+    font-weight: 100;
+    font-style: italic;
+}
+
+@font-face {
+    font-display: fallback;
+    font-family: 'Poppins';
+    src: url('./assets/fonts/Poppins-ExtraLight.woff2') format('woff2'),
+        url('./assets/fonts/Poppins-ExtraLight.woff') format('woff');
+    font-weight: 200;
+    font-style: normal;
+}
+
+@font-face {
+    font-display: fallback;
+    font-family: 'Poppins';
+    src: url('./assets/fonts/Poppins-Regular.woff2') format('woff2'),
+        url('./assets/fonts/Poppins-Regular.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+@font-face {
+    font-display: fallback;
+    font-family: 'Poppins';
+    src: url('./assets/fonts/Poppins-MediumItalic.woff2') format('woff2'),
+        url('./assets/fonts/Poppins-MediumItalic.woff') format('woff');
+    font-weight: 500;
+    font-style: italic;
+}
+
+@font-face {
+    font-display: fallback;
+    font-family: 'Poppins';
+    src: url('./assets/fonts/Poppins-Medium.woff2') format('woff2'),
+        url('./assets/fonts/Poppins-Medium.woff') format('woff');
+    font-weight: 500;
+    font-style: normal;
+}
+
+@font-face {
+    font-display: fallback;
+    font-family: 'Poppins';
+    src: url('./assets/fonts/Poppins-SemiBold.woff2') format('woff2'),
+        url('./assets/fonts/Poppins-SemiBold.woff') format('woff');
+    font-weight: 600;
+    font-style: normal;
+}
+
+@font-face {
+    font-display: fallback;
+    font-family: 'Poppins';
+    src: url('./assets/fonts/Poppins-BlackItalic.woff2') format('woff2'),
+        url('./assets/fonts/Poppins-BlackItalic.woff') format('woff');
+    font-weight: 900;
+    font-style: italic;
+}
+
+@font-face {
+    font-display: fallback;
+    font-family: 'Poppins';
+    src: url('./assets/fonts/Poppins-ExtraLightItalic.woff2') format('woff2'),
+        url('./assets/fonts/Poppins-ExtraLightItalic.woff') format('woff');
+    font-weight: 200;
+    font-style: italic;
+}
+
+@font-face {
+    font-display: fallback;
+    font-family: 'Poppins';
+    src: url('./assets/fonts/Poppins-BoldItalic.woff2') format('woff2'),
+        url('./assets/fonts/Poppins-BoldItalic.woff') format('woff');
+    font-weight: bold;
+    font-style: italic;
+}
+
+@font-face {
+    font-display: fallback;
+    font-family: 'Poppins';
+    src: url('./assets/fonts/Poppins-Light.woff2') format('woff2'),
+        url('./assets/fonts/Poppins-Light.woff') format('woff');
+    font-weight: 300;
+    font-style: normal;
+}
+
+@font-face {
+    font-display: fallback;
+    font-family: 'Poppins';
+    src: url('./assets/fonts/Poppins-Thin.woff2') format('woff2'),
+        url('./assets/fonts/Poppins-Thin.woff') format('woff');
+    font-weight: 100;
+    font-style: normal;
+}
+
+@font-face {
+    font-display: fallback;
+    font-family: 'Poppins';
+    src: url('./assets/fonts/Poppins-Bold.woff2') format('woff2'),
+        url('./assets/fonts/Poppins-Bold.woff') format('woff');
+    font-weight: bold;
+    font-style: normal;
+}
+
+@font-face {
+    font-display: fallback;
+    font-family: 'Poppins';
+    src: url('./assets/fonts/Poppins-Italic.woff2') format('woff2'),
+        url('./assets/fonts/Poppins-Italic.woff') format('woff');
+    font-weight: normal;
+    font-style: italic;
+}
+
+@font-face {
+    font-display: fallback;
+    font-family: 'Paytone One';
+    src: url('./assets/fonts/PaytoneOne-Regular.woff2') format('woff2'),
+        url('./assets/fonts/PaytoneOne-Regular.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+@font-face {
+    font-display: fallback;
+    font-family: 'Coolvetica';
+    src: url('./assets/fonts/coolvetica-rg.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+}
+
+.page-subtitle {
+  font-family: 'Poppins';
+  font-size: 1em;
+}
+.page-subtitleB {
+  font-family: 'Poppins';
+  font-size: 1.5em;
   font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,422 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-carousel hide-delimiters :height="windowSize > 2 ? 500 : 250" continuous cycle>
+      <v-carousel-item
+        v-for="(photo, index) in photos"
+        :key="index"
+        :src="photo.src"
+      >
+      </v-carousel-item>
+    </v-carousel>
+    <v-flex xs12>
+      <v-divider horizontal></v-divider>
+    </v-flex>
+    <!-- Brochure -->
+    <div class="fade-in full-width">
+      <v-layout row wrap>
+        <v-flex md6 xs12 justify-center text-center align-center class=my-auto>
+          <v-flex px-10 xs12>
+            <span class="page-title">Diseño y Impresiónes de Boletines, Menus y Tarjetas de Presentación</span>
+            <v-flex xs12 px-10>
+              <v-divider horizontal></v-divider>
+            </v-flex>
+            <v-flex xs12 pt-10 class="page-text">
+              <span>Contamos con un Equipo de Diseñadores para implementar la solucion que nececites para tu Empresa de Pequeña a Media Escala; como Boletines, Menus, Tarjetas de Presentación, Manuales, Tabloides, Boletos, y mas.. </span>
+            </v-flex>
+          </v-flex>
+        </v-flex>
+        <v-flex md6 text-center py-10 xs12>
+          <img width="70%" style="border-radius: 20px;" src="../assets/training.png"/>
+        </v-flex>
+      </v-layout>
+    </div>
+    <!-- T shirt Quotes -->
+    <div class="fade-in full-width">
+      <v-layout row wrap style="background-color: #F6F6F6">
+        <v-flex xs12>
+          <v-divider horizontal></v-divider>
+        </v-flex>
+        <v-flex text-center xs12 pl-10 pt-10 class="page-title">
+          Impresiónes a Color
+          <v-flex xs4 class="mx-auto" pb-10>
+            <v-divider horizontal></v-divider>
+          </v-flex>
+        </v-flex>
+        <v-flex sm6 md4 lg3 xs12 text-center py-5 v-for="printtype in printTypes" :key="printtype.name">
+          <v-hover v-slot:default="{ hover }">
+            <v-card
+              @click="generateQuoteDialog(printtype.name)"
+              :elevation="hover ? 12 : 2"
+              class="mx-auto"
+              max-width="300"
+              style="border-radius: 25px;"
+            >
+              <v-img class="mx-auto shirts" :src="printtype.src" width="300px">
+                <v-expand-transition>
+                  <div
+                    v-if="hover"
+                    class="d-flex transition-slow-in-fast-out black darken-2 v-card--reveal white--text"
+                    style="height: 100%; border-radius: 25px;"
+                  >
+                   <span class="page-subtitleB">Cotiza Gratis!</span>
+                  </div>
+                </v-expand-transition>
+              </v-img>
+            </v-card>
+          </v-hover>
+        </v-flex>
+        <v-flex xs12 pt-10>
+          <v-divider horizontal></v-divider>
+        </v-flex>
+      </v-layout>
+    </div>
+    <!-- Machines -->
+    <div class="fade-in full-width">
+      <v-layout row wrap pt-10>
+        <v-flex md7 py-10 xs12>
+          <v-img src="../assets/ourmachines.jpg">
+          </v-img>
+          <v-img src="../assets/ourmachines2.jpg">
+          </v-img>
+        </v-flex>
+        <v-flex md5 xs12 text-center pa-10 class="mx-auto">
+          <v-flex xs12 pt-10 class="page-title">
+            Calidad y Durabilidad
+            <v-divider horizontal></v-divider>
+          </v-flex>
+          <v-flex xs12 class="page-text" pt-10>
+            Como empresa priorizamos la calidad y durabilidad de nuestros productos, por lo tanto usamos equipo Profesional para poder brindar productos de alta calidad a nuestros clientes, garantizados.
+          </v-flex>
+        </v-flex>
+        <v-flex xs12 pt-10>
+          <v-divider horizontal></v-divider>
+        </v-flex>
+      </v-layout>
+    </div>
+    <!-- Contact us -->
+    <div class="fade-in full-width">
+      <v-layout row wrap pt-10 style="background-color: #F6F6F6">
+        <v-flex md6 xs12 px-5 py-10>
+          <v-card class="mx-auto" max-width="600px">
+            <v-card-title class="page-title">Buscas imprimir un Diseño?</v-card-title>
+            <v-card-text>
+              <v-layout row pt-5>
+                <v-flex xs12 px-5>
+                  <v-text-field
+                    label="Nombre"
+                    outlined
+                    prepend-inner-icon="mdi-account"
+                  />
+                </v-flex>
+                <v-flex xs12 px-5>
+                  <v-text-field
+                    label="Telefono"
+                    prepend-inner-icon="mdi-phone"
+                    outlined
+                  />
+                </v-flex>
+                <v-flex xs12 px-5>
+                  <v-text-field
+                    prepend-inner-icon="mdi-email"
+                    label="Correo"
+                    outlined
+                  />
+                </v-flex>
+                <v-flex xs12 px-5>
+                  <v-text-field
+                    label="Describe tu Diseño"
+                    prepend-inner-icon="mdi-message"
+                    outlined
+                    height="100px"
+                  />
+                </v-flex>
+              </v-layout>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn dark tile depressed class="text-capitalize" color="green" @click="shirtQuoteDialog = false">Enviar Cotizacion</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+        <v-flex md6 xs12 text-center class="my-auto">
+          <v-flex xs12 class="page-titleLG">
+            Diseña
+          </v-flex>
+          <v-flex xs12 class="page-titleLG">
+            Cotiza
+          </v-flex>
+          <v-flex xs12 class="page-titleLG">
+            Imprime
+          </v-flex>
+          <v-flex pb-10 xs12 class="page-subtitle">
+            IMATEQ.MX
+            <v-flex xs2 class="mx-auto">
+              <v-divider horizontal>
+              </v-divider>
+            </v-flex>
+            <v-flex xs12 pt-2>
+              <a style="text-decoration: none;" href="https://www.facebook.com">
+                <v-icon large color="black">mdi-facebook</v-icon>
+              </a>
+              <a style="text-decoration: none;" href="https://www.instagram.com">
+                <v-icon large color="black">mdi-instagram</v-icon>
+              </a>
+              <a style="text-decoration: none;"  href="https://www.whatsapp.com">
+                <v-icon large color="black">mdi-whatsapp</v-icon>
+              </a>
+            </v-flex>
+          </v-flex>
+        </v-flex>
+        <v-flex xs12 pt-10>
+          <v-divider horizontal></v-divider>
+        </v-flex>
+      </v-layout>
+    </div>
+    <!-- Map -->
+    <div>
+      <v-layout text-center align-center justify-center style="background-color: white">
+        <div class="mapouter">
+          <div class="gmap_canvas">
+            <iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Calle%2016%20de%20Septiembre%20915%2C%20Independencia%2C%2064720%20Monterrey%2C%20N.L.&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+            </iframe>
+          </div>
+        </div>
+      </v-layout>
+    </div>
+    <v-dialog v-model="dialogSuccess" max-width="290" persistent>
+      <v-card>
+        <v-card-title class="headline">Éxito</v-card-title>
+        <v-card-text>Action successful.</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn dark color="green" @click="dialogSuccess = false">Aceptar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="shirtQuoteDialog" max-width="800">
+      <v-card>
+        <v-card-title class="headline">Cotiza tu Impresion a {{ shirtQuoteForm.productName }} !</v-card-title>
+        <v-card-text>
+          <v-form v-model="formIsValid">
+            <v-layout row pt-5>
+              <v-flex xs12 px-5>
+                <v-text-field
+                  :rules="[() => shirtQuoteForm.contactName.length > 0]"
+                  label="Nombre"
+                  outlined
+                  prepend-inner-icon="mdi-account"
+                />
+              </v-flex>
+              <v-flex xs12 px-5>
+                <v-text-field
+                  :rules="[() => shirtQuoteForm.phone.length > 0]"
+                  label="Telefono"
+                  prepend-inner-icon="mdi-phone"
+                  outlined
+                />
+              </v-flex>
+              <v-flex xs12 px-5>
+                <v-text-field
+                  :rules="[() => shirtQuoteForm.email.length > 0]"
+                  prepend-inner-icon="mdi-email"
+                  label="Correo"
+                  outlined
+                />
+              </v-flex>
+              <v-flex xs12 px-5>
+                <v-text-field
+                  :rules="[() => shirtQuoteForm.message.length > 0]"
+                  label="Describe tu Diseño"
+                  prepend-inner-icon="mdi-message"
+                  outlined
+                  height="100px"
+                />
+              </v-flex>
+            </v-layout>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text class="text-capitalize" color="red" @click="shirtQuoteDialog = false">Cancelar</v-btn>
+          <v-btn dark tile depressed class="text-capitalize" color="green" @click="shirtQuoteDialog = false">Enviar Cotizacion</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+  },
+  data: () => ({
+    printTypes: [
+      {
+        name: 'Serigrafia',
+        src: require('../assets/portfolio1.jpg')
+      },
+      {
+        name: 'Transferencia',
+        src: require('../assets/portfolio2.jpg')
+      },
+      {
+        name: 'Vinil Textil',
+        src: require('../assets/portfolio3.jpg')
+      },
+      {
+        name: 'Sublimado',
+        src: require('../assets/portfolio4.jpg')
+      }
+    ],
+    photos: [
+      {
+        src: require('../assets/barniz.jpg')
+      },
+      {
+        src: require('../assets/digital.jpg')
+      },
+      {
+        src: require('../assets/doblez.jpg')
+      },
+      {
+        src: require('../assets/guillotina.jpg')
+      }
+    ],
+    dialogSuccess: false,
+    fadeInElements: [],
+    formIsValid: false,
+    shirtQuoteDialog: false,
+    shirtQuoteForm: {
+      productName: '',
+      contactName: '',
+      email: '',
+      message: '',
+      phone: '',
+      image: ''
+    }
+  }),
+  methods: {
+    generateQuoteDialog (product) {
+      this.shirtQuoteForm.productName = product
+      this.shirtQuoteDialog = true
+    },
+    pushAndResetNav (route) {
+      if (this.$route.path !== route) {
+        this.$router.push(route)
+      }
+    }
+  },
+  computed: {
+    windowSize () {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs': return 0
+        case 'sm': return 1
+        case 'md': return 2
+        case 'lg': return 3
+        case 'xl': return 4
+        default: return 0
+      }
+    }
+  },
+  mounted () {
+    var handleScroll = (evt) => {
+      for (var i = 0; i < this.fadeInElements.length; i++) {
+        var elem = this.fadeInElements[i]
+        if (isElemVisible(elem)) {
+          elem.style.opacity = '1'
+          elem.style.transform = 'scale(1)'
+          this.fadeInElements.splice(i, 1) // only allow it to run once
+        }
+      }
+    }
+    var isElemVisible = (el) => {
+      var rect = el.getBoundingClientRect()
+      var elemTop = rect.top + 200 // 200 = buffer
+      var elemBottom = rect.bottom
+      return elemTop < window.innerHeight && elemBottom >= 0
+    }
+
+    this.fadeInElements = Array.from(document.getElementsByClassName('fade-in'))
+    document.addEventListener('scroll', handleScroll)
+    handleScroll()
+  },
+  unmounted () {
+    var handleScroll = (evt) => {
+      for (var i = 0; i < this.fadeInElements.length; i++) {
+        var elem = this.fadeInElements[i]
+        if (isElemVisible(elem)) {
+          elem.style.opacity = '1'
+          elem.style.transform = 'scale(1)'
+          this.fadeInElements.splice(i, 1) // only allow it to run once
+        }
+      }
+    }
+    var isElemVisible = (el) => {
+      var rect = el.getBoundingClientRect()
+      var elemTop = rect.top + 200 // 200 = buffer
+      var elemBottom = rect.bottom
+      return elemTop < window.innerHeight && elemBottom >= 0
+    }
+    document.removeEventListener('scroll', handleScroll)
   }
 }
 </script>
+
+<style scoped>
+.mapouter{
+  position: relative;
+  text-align:right;
+  height:500px;
+  width:100%;
+}
+.gmap_canvas {
+  overflow:hidden;
+  background:none!important;
+  height:500px;
+  width:100%;
+}
+.page-title {
+  font-family: 'Coolvetica';
+  font-size: 3em;
+}
+.page-titleLG {
+  font-family: 'Coolvetica';
+  font-size: 5em;
+}
+.page-subtitle {
+  font-family: 'Poppins';
+  font-size: 1em;
+}
+.page-subtitleB {
+  font-family: 'Poppins';
+  font-size: 1.5em;
+  font-weight: bold;
+}
+.page-text {
+  font-family: 'Poppins';
+  font-size: 1.5em;
+}
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: .5;
+  position: absolute;
+  width: 100%;
+}
+.shirts {
+  border-radius: 25px;
+  -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+  filter: grayscale(30%);
+}
+.fade-in {
+  opacity: 0;
+  transition: 0.3s all ease-out;
+  transform: scale(0.8);
+  box-sizing: border-box;
+  display: inline-block;
+}
+.full-width{
+  width: 100%;
+}
+</style>
